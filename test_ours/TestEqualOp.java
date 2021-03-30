73bf6230af39dd365a3ff82221511eb612e98771
Poisson Manuel <mpoissonmanuel@gmail.com>
2021-03-30 17:30:28 +0200

    modify test to have test == on int and bool

--- test_ours/TestEqualOp.java
+++ test_ours/TestEqualOp.java
@@ -1,11 +1,18 @@
 class TestEq {
     public static void main(String[] a) {
             /* Boolean testEq = 1 == 1; */
-            if (1==1) {
+            // boolean intEq = 1 == 1;
+            
+            if (true==true) {
+                if (true==true) {
+                //if (1==1) {
+                    System.out.println(2);
+                } else {
+                    System.out.println(3);
+                }
                 System.out.println(2);
             } else {
                 System.out.println(3);
-            }
-
+            }           
     }
 }
